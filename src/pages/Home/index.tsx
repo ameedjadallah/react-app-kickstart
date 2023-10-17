1532209068da8f515a46bbfa6c90781ba9c64156
ameedjadallah <ameed.jadallah@gmail.com>
2023-10-17 15:07:20 +0300

    Fix structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import styles from './Home.module.scss';
+import styles from './index.module.scss';
 import { useTranslation } from 'react-i18next';
 
 const Home = () => {
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,10 @@
+
+import styles from './index.module.scss';
+
+const Login = () => {
+  return (
+    <div className={styles.title}>Login</div>
+  )
+}
+
+export default Login;
